d46f20ddfe87285e7fb0dc49f2d7c2141364ae51
Navit Choudhary <navit.choudhary@yapits.com>
2018-04-14 18:23:20 +1000

    Event manager config update

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -5,7 +5,8 @@
  'use strict';
 
 var mongo = {
-    URI: process.env.MONGO_URI || 'mongodb://localhost/BlockChain_Ticketing',
+    //URI: process.env.MONGO_URI || 'mongodb://localhost/BlockChain_Ticketing',
+    URI: process.env.MONGO_URI || 'mongodb://localhost/BlockChain_Ticketing_Dev',
     //URI: process.env.MONGO_URI || "mongodb://"+process.env.MONGO_USER+":"+process.env.MONGO_PASS+"@localhost/"+process.env.MONGO_DBNAME_DEV,
     port: 27017
 };
